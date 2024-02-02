07d8c237b1e3990c179a8ac50faafdb01f8cc007
sami0880-kea <sami0880@stud.kea.dk>
2024-02-02 10:25:44 +0100

    Completed Exercise 1 in Introduction exercises

---
+++ 00._Assignments/00._Introduction_Exercises/exercise_I.js
@@ -0,0 +1,28 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+const meSentence = `My first name is ${firstName} and my last name is ${lastName}`
+console.log(meSentence)
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2023";
+const increment = 1;
+
+// Add the year plus the increment
+// The result should be 2024
+// You cannot touch line 1 or 2
+const newYear = parseInt(year) + increment;
+console.log(newYear);
+
+
+// --------------------------------------
